bcd83a4b0ffa44048531321db33041bf7b3597c1
Anton Lebedev <lebedew.anton2018@yandex.ru>
2024-07-20 01:33:32 +0400

    first text avava2

--- app/src/main/java/com/example/jetpacktestapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpacktestapp/MainActivity.kt
@@ -3,34 +3,42 @@ package com.example.jetpacktestapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpacktestapp.ui.theme.JetPackTestAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //газ
+        setContent {
+            MyTextStyle()
+        }
     }
 }
 
+@Preview (showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetPackTestAppTheme {
-        Greeting("Android")
+fun MyTextStyle(){
+    Row(
+        modifier = Modifier
+            .background(Color.Blue).fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Газ ебейщ авава")
+        Text(text = "МИШК")
     }
+
 }
